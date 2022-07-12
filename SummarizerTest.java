d7ab1bac9795c579e52fce9faf11ac5c8a4cfb7d
Tichaona Zvidzayi <tzvidzie@gmail.com>
2022-07-12 13:46:08 +0200

    assumption

--- SummarizerTest.java
+++ SummarizerTest.java
@@ -1,8 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+ import org.junit.Test; 
 
-import org.junit.jupiter.api.Test;
+ /*
+  *  ASSUMPTIONS
+Test cases were tricky to test for the CLI without setting up the JUnit in Eclipse for example
+However here is what I sought to achieve;
 
-class SummarizerTest {
+1 - A test fails if there is a mixture if strings/text and numbers
+2 - A Summarizer class is supposed to trim() trailing space e.g. "5 " and convert it to an integer 5
+     Therefore test cases with stings with trailing spaces should PASS.
+3 - If the test case has a single digit its supposed to pass and return the digit.
+4 - Empty or Null strings are supposed to fail.
+5 - The test should fail if the input is of type double or floating, bool. Only integers should be accepted.
+
+  */
+public class SummarizerTest {
 
 	@Test
 	void test() {
@@ -22,7 +33,25 @@ class SummarizerTest {
 		NumberFormatException exception =  assertThrows(NumberFormatException.class, ()-> {
 			summarizer.collect(input);
 		});
-		assertEquals("Input string contains non-digits or is empty", exception.getMessage());
+		assertEquals("Input should contain only intergers", exception.getMessage());
+	}
+
+	@Test
+	public void emptystring() {
+		input = "4.5 , 6.4, 9, 10, 11.0 , 12.23";
+		NumberFormatException exception =  assertThrows(NumberFormatException.class, ()-> {
+			summarizer.collect(input);
+		});
+		assertEquals("Input string should be convertable to integers and cannot be of type floating point /double", exception.getMessage());
+	}
+
+    @Test
+	public void emptystring() {
+		input = "";
+		NumberFormatException exception =  assertThrows(NumberFormatException.class, ()-> {
+			summarizer.collect(input);
+		});
+		assertEquals("Input string cannot be a NULL string", exception.getMessage());
 	}
 
 	@Test
@@ -31,7 +60,7 @@ class SummarizerTest {
 		Collection<Integer> output = summarizer.collect(input);
 		populate(input);
 		assertEquals(expectedCollection, output);
-		clear();
+		
 	}
 	
 	@Test
@@ -40,7 +69,7 @@ class SummarizerTest {
 		Collection<Integer> output = summarizer.collect(input);
 		populate("2,3,4,5,7,10,11,12,13,20,21,22,23,50");
 		assertEquals(expectedCollection, output);
-		clear();
+		
 	}
 
 	@Test
@@ -49,7 +78,7 @@ class SummarizerTest {
 		Collection<Integer> output = summarizer.collect(input);
 		populate("2,3,5,6,7,8,9,13");
 		assertEquals(expectedCollection, output);
-		clear();
+		
 	}
 
 	@Test
@@ -58,8 +87,6 @@ class SummarizerTest {
 		Collection<Integer> output = summarizer.collect(input);
 		assertEquals("1, 3-5, 7, 10-12, 34, 37,40-44", summarizer.summarizeCollection(output));
 	}
-
-
 
 	@Test
 	public void summarizeCollection_test2() {
